23be93e1702cc869af98209dec028688f5e0f2e5
Juan David Posada Paba <jposada@consware.com.co>
2023-03-13 17:51:45 -0500

    Sesión 11 finalizada

--- 04-pipesApp/src/app/app.component.ts
+++ 04-pipesApp/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
+import { PrimeNGModule } from './prime-ng/prime-ng.module';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  
+
+  constructor( private primengConfig: PrimeNGConfig){}
+
+  ngOnInit(): void {
+    this.primengConfig.ripple = true;
+  }
+
   title = 'pipesApp';
+  nombre: string = "JUAn PosaDA"
+  
+  mostrarNombre(){
+    console.log(this.nombre);
+  }
 }
--- 04-pipesApp/src/app/app.module.ts
+++ 04-pipesApp/src/app/app.module.ts
@@ -1,16 +1,41 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRouterModule } from './app-router.module';
+
+//Módulo Personalizado
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
+
+// Cambiar el locale de la app
+
+import localeEsCo from '@angular/common/locales/es-CO'
+import localeFr from '@angular/common/locales/fr'
+
+import { registerLocaleData } from '@angular/common'
+registerLocaleData( localeEsCo );
+registerLocaleData( localeFr );
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouterModule,
+    SharedModule,
+    VentasModule,
+    BrowserAnimationsModule
+    
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-CO' }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+ }
